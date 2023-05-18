37b1a3833e54e82d650ab504d42e5887c6e41840
yamme <lukas.soderman@skola.taby.se>
2023-05-18 03:00:54 +0200

    Co-authored-by: DanielMagnusson <DanielMagnusson@users.noreply.github.com> Co-authored-by: Bentell05 <Bentell05@users.noreply.github.com>

---
+++ js/functions.js
@@ -0,0 +1,30 @@
+function pathing() {
+    for (let i = 0; i < waypoints.length; i++) {
+        if (enemy.position.x === waypoints[i].x && enemy.position.y === waypoints[i].y) {
+            if (waypoints[i].direction == "right") {
+                enemy.speed.x = 1
+                enemy.speed.y = 0
+                console.log("right")
+            } else if (waypoints[i].direction == "up") {
+                enemy.speed.y = -1
+                enemy.speed.x = 0
+                console.log("up")
+            } else if (waypoints[i].direction == "left") {
+                enemy.speed.x = -1
+                enemy.speed.y = 0
+                console.log("left")
+            } else if (waypoints[i].direction == "down") {
+                enemy.speed.y = 1
+                enemy.speed.x = 0
+                console.log("down")
+            }else if (waypoints[i].direction == "dead") {
+                enemy.speed.y = 0
+                enemy.speed.x = 0
+            }
+        }
+    }
+    return(enemy.speed)
+}
+function spawn_enemy(){
+    
+}
---
+++ js/javascript.js
@@ -0,0 +1,97 @@
+const canvas = document.querySelector(".canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.width = 1280
+canvas.height = 576
+
+ctx.fillStyle = "black"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.onload = () => {
+    animation()
+}
+image.src = 'img/towerdefensemap.png'
+
+var x = waypoints[0].x
+var y = waypoints[0].y
+class Enemy{
+    constructor() {
+        this.position={
+            x: waypoints[0].x,
+            y: waypoints[0].y
+        }
+        this.speed={
+            x: 0,
+            y: 0
+        }
+        this.health = 5
+    }
+
+    draw() {
+        ctx.fillStyle = "red"
+        ctx.fillRect(this.position.x, this.position.y, 50, 50)
+    }
+    pathing() {
+        for (let i = 0; i < waypoints.length; i++) {
+            if (this.position.x === waypoints[i].x && this.position.y === waypoints[i].y) {
+                if (waypoints[i].direction == "right") {
+                    this.speed.x = 1
+                    this.speed.y = 0
+                    console.log("right")
+                } else if (waypoints[i].direction == "up") {
+                    this.speed.y = -1
+                    this.speed.x = 0
+                    console.log("up")
+                } else if (waypoints[i].direction == "left") {
+                    this.speed.x = -1
+                    this.speed.y = 0
+                    console.log("left")
+                } else if (waypoints[i].direction == "down") {
+                    this.speed.y = 1
+                    this.speed.x = 0
+                    console.log("down")
+                }else if (waypoints[i].direction == "dead") {
+                    this.speed.y = 0
+                    this.speed.x = 0
+                }
+            }
+        }
+    }
+    update() {
+        this.draw()
+        this.pathing()
+        this.position.y += this.speed.y
+        this.position.x += this.speed.x
+    }
+}
+const enemies=[]
+
+for (let j = 0; j < waypoints.length; j++) {
+    setTimeout(() => {
+        enemies.push(new Enemy());
+    }, 2000 * j)
+}
+    
+
+function animation(){
+    requestAnimationFrame(animation)
+    ctx.clearRect(0,0, canvas.width,canvas.height)
+    ctx.drawImage(image,0,0)
+    
+
+let i = 0
+
+
+    
+    if (i % 100 === 0) {
+        i = i/100
+        enemies.push(enemy())
+    }
+
+    
+    console.log(i)
+    console.log(x,y)
+   i++
+}
+    //animation()
